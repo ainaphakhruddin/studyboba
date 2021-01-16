f231f9cacc29f4f43038a4998ed281f9dc92f3c4
Aina <aina.phakhruddin@gmail.com>
2021-01-16 15:11:55 -0600

    Initial Commit

---
+++ StudyApp/StudyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyAppApp.swift
+//  StudyApp
+//
+//  Created by Aina Phakhruddin on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct StudyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
